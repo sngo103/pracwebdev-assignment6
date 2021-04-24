4e79d0be96b931f6e20621a7c755dbe43cbd5e64
Gabby <gabegabby99@gmail.com>
2021-04-24 16:46:10 -0400

    fixed bug

--- src/components/FoodNameSearch.js
+++ src/components/FoodNameSearch.js
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 
-
 class FoodNameSearch extends React.Component {
   constructor(props) {
     super(props);
@@ -87,83 +86,9 @@ class FoodNameSearch extends React.Component {
         );
       });
     }
-<<<<<<< HEAD
-}
-
-handleInputChange = (event) => {
-  this.setState({meal: event.target.value});
-}
-handleSearchClick = async () => {
-  let mealName = this.state.meal;
-  let linkToAPI = 'https://www.themealdb.com/api/json/v1/1/search.php?s=' + mealName;
-
-  try {
-      let response = await axios.get(linkToAPI);
-      console.log(response.data);
-      this.setState({apiData: response.data, found: true});
-  } catch (error) {
-      if (error.response) {
-          /*
-           * The request was made and the server responded with a
-           * status code that falls out of the range of 2xx
-           */
-          console.log(error.response.data); //Not Found
-          console.log(error.response.status); //404
-          this.setState({found: false});
-      }
- 
-  }
-}
-    // handleSearchClick = () => {
-    //     let mealName = this.state.meal;
-    //     let linkToAPI = 'https://www.themealdb.com/api/json/v1/1/search.php?s='+mealName;
-    //     fetch(linkToAPI)
-    //     .then(res => res.json())
-    //     .then(
-    //       (result) => {
-    //         this.setState({
-    //           isLoaded: true,
-    //           items: result.items
-    //         });
-    //       },
-    //       // Note: it's important to handle errors here
-    //       // instead of a catch() block so that we don't swallow
-    //       // exceptions from actual bugs in components.
-    //       (error) => {
-    //         this.setState({
-    //           isLoaded: true,
-    //           error
-    //         });
-    //       }
-    //     )
-    // }
-
-makeTable = () => {
-  let currData = this.state.apiData;
-  let foundMatch = this.state.found;
-  let table = [];
-  //found is false when we get 404 error
-  console.log("Hiiii");
-  if(!foundMatch){
-      table.push(<tr key={-1}><td>No Results</td></tr>);
-      return table;
-  } else {
-      let origin = currData.strArea;
-      let recipe = currData.strInstructions;
-      table.push(
-        <tr key={currData.id}>
-          <td>Origin: {origin}</td>
-          <td>Recipe: {recipe}</td>
-        </tr>
-      );
-      return table;
-  }
-}
-=======
     return table;
   };
 
->>>>>>> 34f98fb484077f22dc5653a5828fe2783a9ce4d8
   render() {
     return (
       <div>
@@ -176,13 +101,8 @@ makeTable = () => {
               className="rounded-md px-2 py-1 my-1 border-black border-2 text-green-900"
               type="text"
               value={this.state.meal}
-<<<<<<< HEAD
-              onChange={(event)=>this.handleInputChange(event)}
-              placeholder="Enter Meal"
-=======
               onChange={this.handleInputChange}
               placeholder="Enter dish..."
->>>>>>> 34f98fb484077f22dc5653a5828fe2783a9ce4d8
             />
             <br />
             <button
